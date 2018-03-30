c93270305ed2a3eb1826a0cf6a6bc60c09a2da66
Bryan Jimenez <bryan@roguedevs.io>
2018-03-30 22:48:00 +0000

    Made changes into production.rb to allow ckeditor to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # CKeditor
+  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w( ckeditor/* )
+  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
